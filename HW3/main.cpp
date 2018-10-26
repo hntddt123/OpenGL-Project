e4d38239eb0c849438cccbb2266319f04d83e1ad
Nientai Ho <hntddt123@gmail.com>
2018-10-25 22:51:03 -0500

    Initial Commit

---
+++ HW3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW3
+//
+//  Created by Nientai Ho on 2018/10/25.
+//  Copyright © 2018 Nientai Ho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
